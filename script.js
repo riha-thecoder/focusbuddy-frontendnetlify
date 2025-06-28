619f46f532a9436a840f6097d10562f36ff6b801
Riha Kabeer <rihakabeer@Rihas-MacBook-Air.local>
2025-06-27 23:28:24 -0400

    Set BASE_URL to Render backend

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5050';
+const BASE_URL = 'https://focusbuddy-backendrender.onrender.com';
 
 const form = document.getElementById('sessionForm');
 const titleInput = document.getElementById('title');
